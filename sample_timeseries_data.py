cb70bbffac174e182679f828de2efbf14dfbcb09
jpang32 <jpangallozzi@gmail.com>
2022-05-18 21:53:56 -0400

    Successful connection and insertion of sample data to mongodb through insert_data.py

--- sample_timeseries_data.py
+++ sample_timeseries_data.py
@@ -3,39 +3,49 @@ from datetime import timezone
 import datetime
 import json
 import time
+import bson
 
 #result = subprocess.run('ps aux | grep WindowServer', shell=True, stdout=subprocess.PIPE)
 #print(f'{result.stdout.decode("utf-8").split()[2]}')
-process_name = 'WindowServer'
-entry_list = []
 
-for i in range(20):
-	result = subprocess.run(f'ps aux | grep {process_name}', shell=True, stdout=subprocess.PIPE)
-	result = result.stdout.decode('utf-8').split('\n')[0].split()
+def create_sample(num_samples=20, to_file=False):
+	process_name = 'WindowServer'
+	entry_list = []
 
-	dt = datetime.datetime.now(timezone.utc)
-	utc_time = dt.replace(tzinfo=timezone.utc)
-	utc_timestamp = utc_time.timestamp()
+	for i in range(num_samples):
+		result = subprocess.run(f'ps aux | grep {process_name}', shell=True, stdout=subprocess.PIPE)
+		result = result.stdout.decode('utf-8').split('\n')[0].split()
+		
+		dt = ''
+		if to_file:
+			dt = datetime.datetime.now().isoformat()
+		else:
+			dt = datetime.datetime.now()
 
-	entry = {
-		'metadata' : 
-			{'start_date': result[8], 
-			 'cpu_time': result[9], 
-			 'process_name': process_name,
-			 'process_command': ' '.join(result[10:])
-			},
-		'cpu' : result[2],
-		'timestamp' : utc_timestamp
-	}
-	entry_list.append(entry)
+		entry = {
+			'metadata' : 
+				{'start_date': result[8], 
+				'cpu_time': result[9], 
+				'process_name': process_name,
+				'process_command': ' '.join(result[10:])
+				},
+			'cpu' : result[2],
+			'timestamp' : dt
+		}
 
-	print(entry)
 
-	time.sleep(1)
+		entry_list.append(entry)
 
-json_object = json.dumps(entry_list, indent=4)
-print(f'Output json: \n{entry_list}')
+		print(entry)
 
-output_file_name = datetime.datetime.now().strftime('%d%m%Y_%H%M%S')
-with open(f'sample_data/sample_{output_file_name}.json', "w") as outfile:
-    outfile.write(json_object)
+		time.sleep(1)
+
+	print(f'Output entries: \n{entry_list}')
+
+	if to_file:
+		entry_list = json.dumps(entry_list, indent=4)
+		output_file_name = datetime.datetime.now().strftime('%d%m%Y_%H%M%S')
+		with open(f'sample_data/sample_{output_file_name}.json', "w") as outfile:
+			outfile.write(entry_list)
+
+	return entry_list
